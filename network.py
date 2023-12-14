dcfd8b5d20c07befd661d35ab4bc1776b523a536
hoya9802 <hoya9802@gmail.com>
2023-12-14 20:55:50 +0900

    update

--- network.py
+++ network.py
@@ -111,7 +111,7 @@ class FCRN_8S(nn.Module):
         self.conv = nn.Conv2d(3, 64, kernel_size=(7, 7), padding=3, stride=(2, 2)) # [112, 112]
         self.bn = nn.BatchNorm2d(64)
         self.ReLU = nn.ReLU()
-        # self.maxp = nn.MaxPool2d(kernel_size=3, stride=2, padding=1) # [56, 56]
+        self.maxp = nn.MaxPool2d(kernel_size=2, stride=2) # [56, 56]
 
         self.resnet34_part1 = nn.Sequential( # img: [B, 6, 112, 112]
             ResBlock(64, 64),
@@ -119,6 +119,9 @@ class FCRN_8S(nn.Module):
             ResBlock(64, 64),
             ResBlock(64, 128),
             nn.MaxPool2d(kernel_size=2, stride=2),   # [56, 56]
+        )
+
+        self.resnet34_part2 = nn.Sequential(
             ResBlock(128, 128),
             ResBlock(128, 128),
             ResBlock(128, 128),
@@ -126,7 +129,7 @@ class FCRN_8S(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2), # [28, 28]     should be # # [B, 256, 28, 28]
         )
 
-        self.resnet34_part2 = nn.Sequential(
+        self.resnet34_part3 = nn.Sequential(
             ResBlock(256, 256),
             ResBlock(256, 256),
             ResBlock(256, 256),
@@ -136,15 +139,16 @@ class FCRN_8S(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2),  # [14, 14]    # should be # [B, 512, 14, 14]
         )
 
-        self.resnet34_part3 = nn.Sequential(
+        self.resnet34_part4 = nn.Sequential(
             ResBlock(512, 512),
             ResBlock(512, 512),                
             nn.AvgPool2d(kernel_size=2, stride=2),
             nn.Conv2d(512, num_class, kernel_size=(1, 1)) # [7, 7]    # should be # [B, 512, 7, 7]
         )
         
-        self.pred_part1 = nn.Conv2d(256, num_class, kernel_size=(1, 1))
-        self.pred_part2 = nn.Conv2d(512, num_class, kernel_size=(1, 1))
+        self.pred_part1 = nn.Conv2d(128, num_class, kernel_size=(1, 1))
+        self.pred_part2 = nn.Conv2d(256, num_class, kernel_size=(1, 1))
+        self.pred_part3 = nn.Conv2d(512, num_class, kernel_size=(1, 1))
     
         self.up_p3 = nn.ConvTranspose2d(num_class, num_class, kernel_size=4, stride=2, padding=1)
         self.up_p2 = nn.ConvTranspose2d(num_class, num_class, kernel_size=4, stride=2, padding=1)
@@ -154,17 +158,22 @@ class FCRN_8S(nn.Module):
         x = self.conv(x)
         x = self.bn(x)
         x = self.ReLU(x)
-        # x = self.maxp(x)
 
         x = self.resnet34_part1(x)
         x_p1 = self.pred_part1(x)
+        x_p1_14 = self.maxp(self.maxp(x_p1))
+        x_p1_28 = self.maxp(x_p1)
 
         x = self.resnet34_part2(x)
         x_p2 = self.pred_part2(x)
+        x_p2_14 = self.maxp(x_p2)
 
         x = self.resnet34_part3(x)
-        x = self.up_p3(x) + x_p2
-        x = self.up_p2(x) + x_p1
+        x_p3 = self.pred_part3(x)
+
+        x = self.resnet34_part4(x)
+        x = self.up_p3(x) + x_p3 + x_p2_14 + x_p1_14
+        x = self.up_p2(x) + x_p2 + x_p1_28
         x = self.up_p1(x)
 
         return x
